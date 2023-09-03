4058cf451e956e38f21f2b5b8d8577c9576840e8
Mehedi Nayem <naembfh@gmail.com>
2023-09-04 04:28:01 +0800

    second

--- Quill_Spectrum/settings.py
+++ Quill_Spectrum/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'core',
     'category_article',
     'user',
-    'editors_job'
+    'editors_job',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'Quill_Spectrum.urls'
 
